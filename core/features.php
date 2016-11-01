71c59dec27c29c359b431ec24b8ae80679525de5
Eric <emiller@tax-guard.com>
2016-11-01 06:50:53 -0600

    Added bootstrap to the bower.json

--- core/features.php
+++ core/features.php
@@ -2,14 +2,20 @@
 
 class Features {
 	function __construct(){
-		add_filter( 'image_send_to_editor', array($this, 'html5_insert_image'), 10, 9 );
+		add_filter( 'image_send_to_editor', array($this, 'html5_insert_image'), 10, 10);
 	}
-	function html5_insert_image($html, $id, $caption, $title, $align, $url) {
-		$html5 = "<figure id='post-$id media-$id' class='align$align content-image'>";
-		$html5 .= preg_replace('/class=".*?"/', '', $html);;
+	function html5_insert_image($html, $id, $caption, $title, $align, $url, $size, $alt) {
+		$src  = wp_get_attachment_image_src( $id, $size, false );
+		$html5 = "<figure class='align$align content-image'>";
+
+		$html5 .= '<div class="image">';
+			$html5 .= "<img src='$src[0]' alt='$alt' />";
+		$html5 .= "</div>";
+
 		if ($caption) {
 			$html5 .= "<figcaption>$caption</figcaption>";
 		}
+
 		$html5 .= "</figure>";
 		return $html5;
 	}
